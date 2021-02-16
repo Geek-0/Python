2a6219ebc827a193abfb441245ccc3d5b45eb17a
Geek Men <78629784+Geek-0@users.noreply.github.com>
2021-02-16 13:19:41 +0800

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+#注释
+#print()的使用
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print(100)   #这里没有颜色的句子就是注释啦！！
+print(100+100)   #打印100+100的值
+
+print('100+100','=',100+100)
---
+++ input+print.py
@@ -0,0 +1,8 @@
+#python002
+#time：2021，2，16
+
+name = input()  #input()直接让人输入字符
+print('Hello',name)     #name为一个变量，而这个变量由input赋值
+
+name = input('Please enter your name: ')
+print('Hello',name)
